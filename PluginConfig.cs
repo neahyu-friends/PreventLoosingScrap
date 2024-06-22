d9476dbceb7ad8728bd24f240cbfc72a9bb8467e
Vulcanior <admin@vulcanior.fr>
2024-06-22 13:47:06 +0200

    Update de l'api

--- PluginConfig.cs
+++ PluginConfig.cs
@@ -1,35 +1,25 @@
-using Amrv.ConfigurableCompany;
-using Amrv.ConfigurableCompany.content.model;
+using Amrv.ConfigurableCompany.API;
+using UnityEngine;
 
 namespace PreventLoosingScrap {
     public class PluginConfig {
-		public string GenName(string name) {
-			return "neahyu_friends-preventloosingscrap_" + name;
-		}
-
-		public ConfigurationPage PAGE;
-
-		public ConfigurationCategory CATEGORY;
-		public Configuration SCRAP_LOST_RATE_ON_LOOSING;
+		public CPage     PAGE;
+		public CCategory CATEGORY;
+		public CConfig   SCRAP_LOST_RATE_ON_LOOSING;
 
 		public PluginConfig() {
-			PAGE = LethalConfiguration.CreatePage().SetName("VulcaPack").Build();
+			PAGE = ConfigAPI.CreatePageAuto("Prevent Loosing Scrap", "");
 
-			CATEGORY = LethalConfiguration.CreateCategory()
-                .SetID(GenName("category-gameplay"))
-                .SetName("Gameplay")
-				.SetPage(PAGE)
-                .SetColorRGB(255, 0, 0)
-                .HideIfEmpty(false)
-                .Build(); 
+			CATEGORY = ConfigAPI.CreateCategoryAuto("Gameplay", PAGE, Color.red);
 
-			SCRAP_LOST_RATE_ON_LOOSING = LethalConfiguration.CreateConfig(GenName("scrap-rate"))
-				.SetName("Scrap lost rate")
-				.SetCategory(CATEGORY)
-				.SetValue((object)100f)
-				.SetType(ConfigurationTypes.Percent)
-				.SetTooltip(new string[3] { "Percentage of chance to loose item upon party wipe", "", "This rate is the probability of lossing each item inside the ship, with this formula <u>rate <= RAND(0, 100)</u>" })
-				.Build();
+			SCRAP_LOST_RATE_ON_LOOSING = new CConfigBuilder() {
+				ID = "vulcapack-scrap-lost-rate",
+				Name = "Scrap lost rate",
+				BCategory = CATEGORY,
+				DefaultValue = 100f,
+				BTooltip = new string[3] { "Percentage of chance to loose item upon party wipe", "", "This rate is the probability of lossing each item inside the ship, with this formula <u>rate <= RAND(0, 100)</u>" },
+				Type = CTypes.WholePercent()
+			}.Build();
 		}
     }
 }
